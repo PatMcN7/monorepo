e31bd226b450611dcb820a2b8049bd0e37178aa8
LelsersLasers <millankumar@gmail.com>
2026-09-12 17:30:19 -0400

    send pecs

--- firmware/source/a_box/main.c
+++ firmware/source/a_box/main.c
@@ -183,6 +183,7 @@ void bms_task(void) {
     // ADBMS
     adbms_periodic(&g_bms, MIN_V_FOR_BALANCE, MIN_DELTA_FOR_BALANCE);
 
+    // CAN
     uint16_t pack_voltage = (uint16_t)(g_bms.sum_voltage * PACK_COEFF_PACK_ANALOG_PACK_VOLTAGE);
     uint16_t min_cell_voltage = (uint16_t)(g_bms.min_voltage * PACK_COEFF_PACK_BMS_MIN_CELL_VOLTAGE);
     uint16_t max_cell_voltage = (uint16_t)(g_bms.max_voltage * PACK_COEFF_PACK_BMS_MAX_CELL_VOLTAGE);
@@ -190,6 +191,31 @@ void bms_task(void) {
     uint8_t avg_temp = (uint8_t)(g_bms.avg_therm_temp);
     CAN_SEND_pack_bms(pack_voltage, min_cell_voltage, max_cell_voltage, max_temp, avg_temp);
     CAN_SEND_pack_bms_ccan(pack_voltage, min_cell_voltage, max_cell_voltage, max_temp, avg_temp);
+
+    CAN_SEND_bms_pecs(
+        g_bms.err_cell_voltage_pecs[0],
+        g_bms.err_cell_voltage_pecs[1],
+        g_bms.err_cell_voltage_pecs[2],
+        g_bms.err_cell_voltage_pecs[3],
+        g_bms.err_cell_voltage_pecs[4],
+        g_bms.err_cell_voltage_pecs[5],
+        g_bms.err_gpio_voltage_pecs[0],
+        g_bms.err_gpio_voltage_pecs[1],
+        g_bms.err_gpio_voltage_pecs[2],
+        g_bms.err_gpio_voltage_pecs[3]
+    );
+    CAN_SEND_bms_pecs_ccan(
+        g_bms.err_cell_voltage_pecs[0],
+        g_bms.err_cell_voltage_pecs[1],
+        g_bms.err_cell_voltage_pecs[2],
+        g_bms.err_cell_voltage_pecs[3],
+        g_bms.err_cell_voltage_pecs[4],
+        g_bms.err_cell_voltage_pecs[5],
+        g_bms.err_gpio_voltage_pecs[0],
+        g_bms.err_gpio_voltage_pecs[1],
+        g_bms.err_gpio_voltage_pecs[2],
+        g_bms.err_gpio_voltage_pecs[3]
+    );
 
     bool is_bms_disconnected = g_bms.state != ADBMS_STATE_CONNECTED;
     update_fault(FAULT_ID_BMS_DISCONNECTED, is_bms_disconnected);
